b72abd890d8b9b968a23e7acca8b72e61eb81a29
Morten Holt <thawk@t-hawk.com>
2016-02-11 22:36:50 +0100

    First outline... Needs refactoring to be test-able

---
+++ radio.js
@@ -0,0 +1,36 @@
+/**
+ * Created by morten on 11-02-16.
+ */
+
+'use strict';
+
+var http = require('http');
+var parser = require('htmlparser2');
+
+var dates = [
+    "160205"
+];
+
+var times = [
+    "03"
+];
+
+dates.forEach((date) => {
+    times.forEach((time) => {
+        
+
+        var body;
+        http.request(`http://radioabc.dk/playlist/default.asp?dato=${date}&time=${time}`, (res) => {
+            console.log(`STATUS: ${res.statusCode}`);
+            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
+            res.on('end', () => {
+
+            })
+        }).end();
+    });
+});
+

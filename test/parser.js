051863b81aacf5d29038309c91f525442bfbcbbc
Morten Holt <thawk@t-hawk.com>
2016-03-09 21:41:04 +0100

    Added song splitting on dashes

--- test/parser.js
+++ test/parser.js
@@ -51,32 +51,27 @@ describe('parser', function() {
        }));
 
        it("should find divs with bottom margin only", sinon.test(function(done) {
-           parser.parseHtml('<div><div style="margin-top: 6px; margin-bottom: 10px; font-weight: bold;"><span>Test</span>Test 2</div><div style="margin-bottom: 2px;"><span>Test 3</span>Test 4</div></div>',  (res) => {
+           var song = "Test 4";
+           parser.parseHtml(`<div><div style="margin-top: 6px; margin-bottom: 10px; font-weight: bold;"><span>Test</span>Test 2</div><div style="margin-bottom: 2px;"><span>Test 3</span>${song}</div></div>`,  (res) => {
                res.should.eql([
                    {
-                       'inDiv': false,
-                       'inSpan': false,
                        'time': 'Test 3',
-                       'song': 'Test 4'
+                       'song': song
                    }
                ]);
 
                done();
-           });
+           });s
        }));
 
        it("should find multiple songs", sinon.test(function(done) {
            parser.parseHtml('<div><div style="margin-bottom: 2px;"><span>Test</span>Test 2</div><div style="margin-bottom: 2px;"><span>Test 3</span>Test 4</div></div>',  (res) => {
                res.should.eql([
                    {
-                       'inDiv': false,
-                       'inSpan': false,
                        'time': 'Test',
                        'song': 'Test 2'
                    },
                    {
-                       'inDiv': false,
-                       'inSpan': false,
                        'time': 'Test 3',
                        'song': 'Test 4'
                    }
@@ -88,11 +83,28 @@ describe('parser', function() {
    });
 
     describe("splitTitle", function() {
+        it("should split on dash, with spaces", function() {
+            parser.splitTitle("Artist - Title").should.eql(["Artist", "Title"]);
+        });
 
+        it("should split on dash, without spaces", function() {
+           parser.splitTitle("Artist-Title").should.eql(["Artist","Title"]);
+        });
+
+        it("should split on spaced dash, if present", function() {
+            parser.splitTitle("Long-Artist - Title").should.eql(["Long-Artist","Title"]);
+        });
+
+        it("should split on the first spaced, or unspaced, dash", function() {
+            parser.splitTitle("Artist - Title - Remix").should.eql(["Artist","Title - Remix"]);
+            parser.splitTitle("Artist - Title-Remix").should.eql(["Artist","Title-Remix"]);
+            parser.splitTitle("Artist-Title-Remix").should.eql(["Artist","Title-Remix"]);
+            parser.splitTitle("Long-Artist - Title-Remix").should.eql(["Long-Artist","Title-Remix"]);
+            parser.splitTitle("Long-Artist - Title - Remix").should.eql(["Long-Artist","Title - Remix"]);
+        });
     });
 
     describe("parseArtist", function() {
-
     });
 
     describe("parseTitle", function() {
